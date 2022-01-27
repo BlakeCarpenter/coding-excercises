0064b050bc0a252abc7c88213eb20836e201e834
Blake Carpenter <wbcarpenter066@gmail.com>
2022-01-27 13:03:00 -0500

    Updating `test` to include color output

--- util/testUtil.ts
+++ util/testUtil.ts
@@ -1,9 +1,9 @@
 export function test(expected, recieved):boolean{
     if(expected == recieved){
-        console.log("Assertation passes");
+        console.log("\x1b[32m", "Assertation passes", "\x1b[37m");
         return true;
     } else {
-        console.log(`Expected ${expected}, got ${recieved}`);
+        console.log("\x1b[31m", `Expected ${expected}, got ${recieved}`, "\x1b[37m");
         return false;
     }
 }
