ce4c838cef1494d326b090f95d69cec4ad2889aa
Blake Carpenter <wbcarpenter066@gmail.com>
2022-01-26 22:52:44 -0500

    Old problem from 1/6

---
+++ Daily_20220106/Daily_20220126.ts
@@ -0,0 +1,23 @@
+import {test} from "../util/testUtil";
+
+function main(){
+    test(3, witnesses([3,6,3,4,1]));
+    test(2, witnesses([7,1,2,3,4,5]));
+    test(1, witnesses([1,2,3,4,5,6,7]));
+    test(0, witnesses([]));
+}
+
+function witnesses(heights:number[]):number{
+    if(!heights.length) return 0;
+    let max:number = 0;
+    let viewers:number = 0;
+    for(let i:number = heights.length; i >= 0; i--){
+        if(heights[i] > max){
+            max = heights[i];
+            viewers++;
+        }
+    }
+    return viewers;
+}
+
+main()
