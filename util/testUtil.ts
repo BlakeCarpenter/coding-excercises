3e698ffc517d59c85e2f99154c81cdfd5cdbc0d9
Blake Carpenter <wbcarpenter066@gmail.com>
2022-02-02 01:00:00 -0500

    Tweaking test to accommodate arrays

--- util/testUtil.ts
+++ util/testUtil.ts
@@ -1,9 +1,31 @@
 export function test(expected, recieved):boolean{
-    if(expected == recieved){
-        console.log("\x1b[32m", "Assertation passes", "\x1b[37m");
-        return true;
-    } else {
-        console.log("\x1b[31m", `Expected ${expected}, got ${recieved}`, "\x1b[37m");
-        return false;
+    if(Array.isArray(expected)){
+        if(!Array.isArray(recieved)) return fail(`Expected array, got ${recieved}`);
+        return testArray(expected as [], recieved as []) ?
+            pass("Assertation passes") :
+            fail(`Arrays are not equal, expected\n[${expected}]\ngot\n[${recieved}]`);
     }
+    if(expected == recieved){
+        return pass("Assertation passes");
+    } else {
+        return fail(`Expected ${expected}, got ${recieved}`);
+    }
+}
+
+function testArray(expected:[], recieved:[]):boolean{
+    if(expected.length != recieved.length) return false;
+    for(let i:number = 0; i < expected.length; i++){
+        if(expected[i] != recieved[i]) return false;
+    }
+    return true;
+}
+
+function pass(msg:string):boolean{
+    console.log("\x1b[32m", msg, "\x1b[37m");
+    return true;
+}
+
+function fail(msg:string):boolean{
+    console.log("\x1b[31m", msg, "\x1b[37m");
+    return false;
 }
